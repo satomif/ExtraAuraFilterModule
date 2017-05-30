8698e88e5c82705ba028a04b4e205cb4807edc68
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-30 16:22:21 +0900

    remove unnecessary cleanup job

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,15 +3,3 @@
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /* @var $loader \Composer\Autoload\ClassLoader */
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-
-$_ENV['TMP_DIR'] = __DIR__ . '/tmp';
-$unlink = function ($path) use (&$unlink) {
-    foreach (glob(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '*') as $file) {
-        is_dir($file) ? $unlink($file) : unlink($file);
-        @rmdir($file);
-    }
-};
-$unlink($_ENV['TMP_DIR']);
-register_shutdown_function(function () use ($unlink) {
-    $unlink($_ENV['TMP_DIR']);
-});
